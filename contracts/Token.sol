e80b70767e40556f208ea5ec6759b7ebae21e603
Carlos Colorado <carlos.colorado@gmail.com>
2018-10-31 14:42:35 -0600

    Makes token mutable once
    * Adds Token migration
    * Adds dependencies

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.25;
+pragma solidity ^0.4.24;
 
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
@@ -8,12 +8,25 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20Pausable.sol";
 
 contract Token is ERC20, ERC20Mintable, ERC20Pausable, Ownable{
 
-  string name;
-  string symbol;
+  string public name;
+  string public symbol;
+  bool private canChange;
 
   constructor() public {
     name = "tokenName";
     symbol = "TN";
+    canChange = true;
+  }
+
+  modifier changable() {
+    require( canChange );
+    _;
+  }
+
+  function mutateToken(string _name, string _symbol) onlyOwner changable public {
+    name = _name;
+    symbol = _symbol;
+    canChange = false;
   }
 
 }
